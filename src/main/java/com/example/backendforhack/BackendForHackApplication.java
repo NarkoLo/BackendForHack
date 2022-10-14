7b6d9667faee8d895430202d2c8bf5f541bcd93f
narko <nekit.2280.3390@gmail.com>
2022-10-14 21:16:41 +0300

    First Commit

---
+++ src/main/java/com/example/backendforhack/BackendForHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendforhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendForHackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendforhack/BackendForHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendforhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
